b89bd2bd51a84590d61b962b1e5fc449e774d687
Jinsoo Bae <beajinsu@gmail.com>
2025-10-16 19:36:56 +0900

    오랜만의 자동화 업데이트

--- scripts/markers.js
+++ scripts/markers.js
@@ -2,22 +2,17 @@ var markers = [
   {
     lat: 35.6895,
     lng: 139.6917,
-    popup: "도쿄 여행기",
-    tooltip: "도쿄 여행기",
-    link: "https://polarsteps.com/yourtrip_tokyo"
+    popup: "도쿄 방문",
+    tooltip: "Tokyo",
+    link: "https://findpenguins.com/4gi09jnrjw6mk/trip/68c8c639dfd214-43110362-2?sort=ASC",
+    year: 2025
   },
   {
-    lat: 48.8566,
-    lng: 2.3522,
-    popup: "파리 여행기",
-    tooltip: "파리 여행기",
-    link: "https://journey.app/shared/paris-trip"
-  },
-  {
-    lat: 37.3891,
-    lng: -5.9845,
-    popup: "세비야 스냅사진",
-    tooltip: "세비야 스냅사진",
-    link: "https://findpenguins.com/trip_sevilla"
+    lat: 40.7128,
+    lng: -74.0060,
+    popup: "25.09 뉴욕 여행",
+    tooltip: "New York Trip",
+    link: "https://findpenguins.com/4gi09jnrjw6mk/trip/68d95802bcd6a8-38765511?sort=ASC",
+    year: 2025   // ← 이렇게 명시 (25.09처럼 텍스트에서 추출하지 말고 year 필드로)
   }
 ];
